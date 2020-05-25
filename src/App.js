85db6355e04f4094f42e8968c7d291d3f0389c2e
aidanh5 <58790190+aidahn845@users.noreply.github.com>
2020-05-25 18:03:54 -0400

    dev 2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { Nav, Navbar, NavItem, NavLink } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+import { onError } from "./libs/errorLib";
+
 
 function App() {
+  const history = useHistory();
+
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+
+    userHasAuthenticated(false);
+
+    history.push("/login");
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
-      <Navbar fluid collapseOnSelect>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
+      <Navbar bg="light" fluid collapseOnSelect expand="md">
+        <Navbar.Brand href="/">Scratch</Navbar.Brand>
+        <Navbar.Toggle />
         <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+          <Nav className="ml-auto">
+
+            {isAuthenticated
+              ?
+                <React.Fragment>
+                  <Nav.Link href="/settings">Settings</Nav.Link>
+                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                </React.Fragment>
+                
+              : <React.Fragment>
+                <Nav.Link href="/signup">Signup</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+              </React.Fragment>
+            }
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
+
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "YOUR_STRIPE_PUBLIC_KEY",
+  
   s3: {
     REGION: "us-east-2",
     BUCKET: "yh-notes-uploads"
